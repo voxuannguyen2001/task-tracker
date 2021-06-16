abbac61b42284dc756e55b5c4b73bbad907cca8a
Nguyen Vo <voxuannguyen2001@gmail.com>
2021-06-16 11:02:07 +0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+import { useLocation } from 'react-router-dom'
+
+const Header = ({title, onAdd, showAdd}) => {
+	const location = useLocation()
+
+	return (
+		<header className='header'>
+				<h1>{title}</h1>
+				{location.pathname === '/' &&
+					(<Button 
+					text={showAdd ? 'Close' : 'Add'}
+					color={showAdd ? 'red' : 'green'}
+					onClick={onAdd} />)
+				}
+		</header>
+	)
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+		toggleShowAddTask: PropTypes.func
+}
+
+export default Header
